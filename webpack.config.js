aea47b3bca67fbfdca350018aabfb6caccbe1c87
EmmettMartinBig <emmett@bigdigital.ca>
2024-10-15 10:25:28 -0400

    express for brighsign forum

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ZipPlugin = require('zip-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    publicPath: './',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  target: 'node',
+
+  resolve: {
+    extensions: ['.html', '.js']
+  },
+  externals: {
+    '@brightsign/screenshot': 'commonjs @brightsign/screenshot'
+  },
+  module: {
+    rules: [{}]
+  },
+  plugins: [
+    new ZipPlugin({
+      filename: 'node-server.zip'
+    })
+  ]
+};
